29b803e8208bce6bb1888f51b0dc25dc1649e4e3
vishalmohanty <vishalmohanty97@gmail.com>
2022-02-19 20:30:25 -0800

    Add chrome storage for caching responses

--- src/background.js
+++ src/background.js
@@ -4,35 +4,52 @@
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log('Reached background');
+    const webpage = request.webpage;
 
-    // Get breach information from haveibeenpwned
-    url = 'https://haveibeenpwned.com/api/v3/' + request.service + '/' + request.webpage;
-    const promise1 = fetch(url, {
-        headers: {
-            'Accept': 'application/json'
+    chrome.storage.local.get(webpage, function(data) {
+        if (data[webpage] !== undefined) {
+            console.log('Found data in storage')
+            sendResponse(data[webpage])
+        } else {
+            console.log('Did not find data in storage')
+            // Get breach information from haveibeenpwned
+            const url = 'https://haveibeenpwned.com/api/v3/' + request.service + '/' + webpage;
+            const promise1 = fetch(url, {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .catch(error => console.log('Error:', error));
+
+            // Get news articles by searching "webpage breach"
+            const news_api_key = 'a9d2c1e7fba74c8db7d7c2a23c10aa9e';
+            const news_url = 'https://newsapi.org/v2/everything?q=' + webpage + '%20breach&apiKey=' + news_api_key;
+            const promise2 = fetch(news_url, {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .catch(error => console.log('Error:', error));
+
+            Promise.all([promise1, promise2]).then(data => {
+                const dataFromPromise1 = data[0];
+                const dataFromPromise2 = data[1];
+                console.log(dataFromPromise1);
+                console.log(dataFromPromise2);
+                const json_obj = {
+                    "hibp": dataFromPromise1,
+                    "recent_news": dataFromPromise2
+                };
+                var obj = {}
+                obj[webpage] = json_obj
+                chrome.storage.local.set(obj, function(){
+                    console.log("Added to storage: ", obj)
+                });
+                sendResponse(json_obj);
+            });
         }
-    })
-    .then(response => response.json())
-    .catch(error => console.log('Error:', error));
-
-    // Get news articles by searching "webpage breach"
-    news_api_key = 'a9d2c1e7fba74c8db7d7c2a23c10aa9e';
-    news_url = 'https://newsapi.org/v2/everything?q=' + request.webpage + '%20breach&apiKey=' + news_api_key;
-    const promise2 = fetch(news_url, {
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .catch(error => console.log('Error:', error));
-
-    Promise.all([promise1, promise2]).then(data => {
-        const dataFromPromise1 = data[0]
-        const dataFromPromise2 = data[1]
-        sendResponse({
-            hibp: dataFromPromise1,
-            recent_news: dataFromPromise2
-        })
     })
     
     return true;
